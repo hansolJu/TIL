93be2575569fa101d4552b9894d13f5b5f6351e5
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2019-07-21 20:02:53 +0900

    1.1.2 값으로써의 함수와 클로져

--- javascript/FunctionalJavascriptProgramming/1/addMaker.js
+++ javascript/FunctionalJavascriptProgramming/1/addMaker.js
@@ -18,4 +18,8 @@ var v2 = function () { };
 function f1() { return 100; }
 function f2() { return function () { }; }
 
-
+//코드가 실행된후, 어디서도 addMaker의 인자인 a값을 변경시키지 않고 있기 때문에 항상 동일한 값을 갖는다. 때문에 위 상황에서 a는 불변하며 상수로 쓰이게된다. 
+//이 상황에서의 a는 불변하지만, 모든 경우의 클로저가 그렇지는 않다. 클로저가 기억하는 변수의 값은 변할 수 있다.
+var add3 = addMaker(3);
+add3(3); // 6
+add3(4); // 7
